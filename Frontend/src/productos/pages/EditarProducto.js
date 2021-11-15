9a360bb5a6bdf01326fd740598517adc93cb3bd7
PaulaJulianaPS <90658689+PaulaJulianaPS@users.noreply.github.com>
2021-11-15 17:17:40 -0500

    Update EditarProducto.js

--- Frontend/src/productos/pages/EditarProducto.js
+++ Frontend/src/productos/pages/EditarProducto.js
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useParams, useHistory } from "react-router-dom";
+import React, { useState } from "react";
 import { Container, Row, Col, Alert } from "react-bootstrap";
 import api from "../../api";
 import ProductForm from "../components/ProductForm";
 
-const EditarProducto = ({ productos, setProductos }) => {
-  const history = useHistory();
+const CrearProducto = ({ productos, setProductos }) => {
   const categorias = [
-    { id: 1, nombre: "Moderno" },
-    { id: 2, nombre: "Informal" },
-    { id: 3, nombre: "Bohemio" },
-    { id: 4, nombre: "Clasico" },
-    { id: 5, nombre: "Deportes" },
-    { id: 6, nombre: "Elegante" },
-    { id: 7, nombre: "Casual" },
-    { id: 8, nombre: "Juvenil" },
+    { id: 1, nombre: "Tubérculo" },
+    { id: 2, nombre: "Frutas" },
+    { id: 3, nombre: "Lácteos" },
+    { id: 4, nombre: "Huevos" },
+    { id: 5, nombre: "Vegetales" },
   ];
   const [error, setError] = useState();
   const [success, setSuccess] = useState();
-  const { productId } = useParams();
 
   const [newProduct, setNewProduct] = useState({
     title: "",
@@ -29,34 +23,25 @@ const EditarProducto = ({ productos, setProductos }) => {
     disponible: false,
   });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await api.products.getProduct(productId);
-      setNewProduct(response);
-    };
-
-    fetchData();
-  }, [productId]);
-
   const handleChange = (event) => {
     setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
   };
 
   const handleClick = async () => {
-    const apiResponse = await api.products.edit(newProduct);
+    const apiResponse = await api.products.create(newProduct);
     if (apiResponse.err) {
       setError(apiResponse.err.message);
       console.log(apiResponse.err);
     } else {
       setSuccess(apiResponse);
       setProductos([...productos, newProduct]);
-      history.push("/Gestion");
+      //history.push("/");
     }
   };
 
   return (
     <React.Fragment>
-      <h1 className="text-center mt-5 mb-5">Editar producto</h1>
+      <h1 className="text-center mt-5 mb-5">Crear producto</h1>
       <Container>
         <Row className="d-flex justify-content-center align-items-center">
           <Col xs={6}>
@@ -75,4 +60,5 @@ const EditarProducto = ({ productos, setProductos }) => {
   );
 };
 
-export default EditarProducto;
+export default CrearProducto;
+
